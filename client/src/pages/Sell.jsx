a443afab7a9b25d1442295ad8977bbfbe9d1ffd7
devendra000001 <devendrakhatke20@gmail.com>
2025-12-24 23:12:50 +0530

    Create Sell.jsx

---
+++ client/src/pages/Sell.jsx
@@ -0,0 +1,9 @@
+import SellForm from "../components/SellForm";
+
+export default function Sell() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SellForm />
+    </div>
+  );
+}
